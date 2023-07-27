3e4b45588a96bbaab9423d2668a181953035346a
smendescoti <sergio.coti@gmail.com>
2023-07-26 21:08:12 -0300

    feat: Configuração do Swagger e CORS

--- ProductsAPI.Services/Program.cs
+++ ProductsAPI.Services/Program.cs
@@ -2,23 +2,24 @@ using MediatR;
 using ProductsAPI.Application.Interfaces.Services;
 using ProductsAPI.Application.Services;
 using ProductsAPI.Infra.IoC.Extensions;
+using ProductsAPI.Services.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerDoc(); //Swagger
 builder.Services.AddSqlServerConfig(builder.Configuration); //SqlServer
 builder.Services.AddMongoDBConfig(builder.Configuration); //MongoDB
 builder.Services.AddDependencyInjection(); //Serviços
 builder.Services.AddMediatRConfig(); //MediatR
 builder.Services.AddJwtBearer(builder.Configuration); //JWT   
+builder.Services.AddCorsPolicy(); //CORS
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerDoc(); //Swagger
+app.UseCorsPolicy(); //CORS
 
 app.UseAuthentication();
 app.UseAuthorization();
